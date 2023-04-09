26df1d2ffe57d55e2a9887c8aaf3329b09df71e8
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-04-09 17:06:02 +0500

    firebase-fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_APP_ID
+	apiKey: 'AIzaSyAQtNdrVXYhQFwxKJ4ShcwhzbvVkWt8ydE',
+	authDomain: 'auth-test-ab8a4.firebaseapp.com',
+	projectId: 'auth-test-ab8a4',
+	storageBucket: 'auth-test-ab8a4.appspot.com',
+	messagingSenderId: '798609792702',
+	appId: '1:798609792702:web:1084973788111cde4148ef'
 };
 
 const app = initializeApp(firebaseConfig);
